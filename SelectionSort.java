f28ea52677a03098e091967c3780d6299739ca26
Riko <r_hamblin@outlook.com>
2016-12-02 02:12:43 -0500

    add addition functionality

--- SelectionSort.java
+++ SelectionSort.java
@@ -8,23 +8,15 @@
  */
 public class SelectionSort {
 
-    public static void main(String[] args) {
-        int[] arr = {10, 20, 5, 6, 1};
-
-        SelectionSort.selectionSort(arr);
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-    }
-
+    static public final int ASCENDING = 0;
+    static public final int DECENDING = 1;
     /**
-     * Effects: This method sorts an array of integers using selection sort
-     * technique
+     * Effects: This method sorts an array of integers in ascending order using selection sort technique
      *
      * @param arr
      */
     static public void selectionSort(int[] arr) {
+
         /*
          *This technique has two iterators. The first one sits at a position in the array. The second iterator then
          * traverses through the remaining of the array to find the index of a smaller number if it exist. 
@@ -46,7 +38,7 @@ public class SelectionSort {
                     smallestIndex = j;
                 }
             }
-            
+
             if (smallestIndex != i) { //swap only if index changed
                 temp = arr[i];
                 arr[i] = arr[smallestIndex];
@@ -55,4 +47,55 @@ public class SelectionSort {
         }
 
     }
+
+    /**
+     * Effects: This method sorts an array of integers in the order specified using selection sort technique
+     *
+     * @param arr
+     * @param selection
+     */
+    static public void selectionSort(int[] arr, int selection) {
+        
+        if(selection == SelectionSort.ASCENDING) {
+            SelectionSort.selectionSort(arr);
+            return;
+        }
+        
+        int j, biggestIndex = 0,temp; 
+
+        for (int i = 0; i < arr.length - 1; i++) { //the last element doesnt need to be visited. At that point the array would be sorted
+
+            biggestIndex = i; //assume your starting position is the smallest index
+
+            for (j = i + 1; j < arr.length; j++) //for the rest of the sub array determine the smallest index
+            {
+                if (arr[j] > arr[biggestIndex]) {
+                    biggestIndex = j;
+                }
+            }
+
+            if (biggestIndex != i) { //swap only if index changed
+                temp = arr[i];
+                arr[i] = arr[biggestIndex];
+                arr[biggestIndex] = temp;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 5, 5, 1};
+
+        SelectionSort.selectionSort(arr,SelectionSort.DECENDING);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        
+        System.out.println();
+        SelectionSort.selectionSort(arr,SelectionSort.ASCENDING);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
 }
